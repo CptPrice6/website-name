c64de0325ed95f33d8c01f0bf2b9d9f5ea9c3a75
Domantas <105386962+CptPrice6@users.noreply.github.com>
2025-02-22 20:19:06 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
-# download needed things
+# download needed things (docker, docker-compose, git, composer, npm, php, nodejs, react etc)
+
+# RUN composer install in backend
+# RUN npm install in frontend
+
+# ADD .env file
+
 # docker-compose -p website-name up --build -d
