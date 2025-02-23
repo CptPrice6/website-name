c2172a50b92d8b0223f7326b7d5d022df8a3cab5
Domantas <domasp902@gmail.com>
2025-02-23 16:06:35 +0200

    corrected the logic of backend external port determination in react-app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ const isLocalhost = window.location.hostname === "localhost";
 const API_BASE_URL = isLocalhost
   ? "http://localhost:8000"
   : `http://${window.location.hostname}:${
-      process.env.BACKEND_EXTERNAL_PORT || 8000
+      process.env.REACT_APP_BACKEND_EXTERNAL_PORT || 8000
     }`;
 
 function HomePage() {
