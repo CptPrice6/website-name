d20121e4dc3458cbca55d97b5701a568a6c7bc4e
Domantas <domasp902@gmail.com>
2025-02-23 11:56:38 +0200

    added .env to a commit for testing, started writing setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
-# download prerequisites and other needed things (docker, docker-compose, git, composer, npm, php, nodejs, react etc)
+#!/bin/bash
+
+# prerequisites: sudo apt update, sudo apt install git
+sudo apt install apt-transport-https ca-certificates curl software-properties-common lsb-release -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # ADD .env file
 
